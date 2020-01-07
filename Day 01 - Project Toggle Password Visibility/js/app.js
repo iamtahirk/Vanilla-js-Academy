5e1b49838926c1a9b7372e24ee2120f9fe749bbe
Tahir <muhammadtahirk@gmail.com>
2020-01-07 11:57:23 +0500

    addEventListner instead of onChange

--- Day 01 - Project Toggle Password Visibility/js/app.js
+++ Day 01 - Project Toggle Password Visibility/js/app.js
@@ -1,10 +1,18 @@
-const pwdCheckBox = document.querySelector('input[id="show-password"]');
-const pwdField = document.querySelector('input[id="password"]');
+const pwdCheckBox = document.querySelector('#show-password');
+const pwdField = document.querySelector('#password');
 
-pwdCheckBox.onchange = function() {
+// pwdCheckBox.onchange = function() {
+//   if (pwdCheckBox.checked) {
+//     pwdField.type = 'text';
+//   } else {
+//     pwdField.type = 'password';
+//   }
+// };
+
+pwdCheckBox.addEventListener('click', () => {
   if (pwdCheckBox.checked) {
     pwdField.type = 'text';
   } else {
     pwdField.type = 'password';
   }
-};
+});
